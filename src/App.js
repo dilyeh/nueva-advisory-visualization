b73ef90acb7bcb163db5348aeed56cab24183c01
Dillon <dilyeh@nuevaschool.org>
2025-05-06 09:23:02 -0700

    this is very broken right now. I tried to make animation work, but it's not really working right now, so I'm gonna try to get a simpler example working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
-import {Dot, Vector2, Visualization} from './helpers.js';
+import {Visualization, VisualizationContainer} from './helpers.js';
 
 
 function App() { // im not very familiar with what best practices exist for js/react, so App() is just gonna be main()
+  // stuff to rerender stuff
+  const visualizationRef = useRef(new VisualizationContainer()); // instantiate the vis
+  const [tick, setTick] = useState(0); // used to trigger rerenders
 
-  // generate the dots
-  let visualizationDots = [];
-  for (let i=0; i<5; i++) {
-    visualizationDots.push(new Dot(new Vector2(i * 10, i * 10)));
-  }
+  useEffect(() => { // this was chatgpted because i don't understand react hooks
+    const interval = setInterval(() => {
+      visualizationRef.current.updateVisualization(); // WHY DO I NEED THE KEYWORD CURRENT????
+      setTick(prev => prev + 1); // what is this sorcery???
+    }, 1000);
 
-  return (
+    return () => clearInterval(interval); // Clean up on unmount
+  }, []);
+
+  return ( // the getValue() thing is sketchy.....
     <div className="App">
       <h1>Advisory Visualization</h1>
         <div class="visualization-container">
           this is a visualization visualization visualization visualization visualization visualization visualization visualization visualization 
-          <Visualization visualizationDots={visualizationDots}/>
+          <Visualization visualizationDots={visualizationRef.current}/>
         </div>
     </div>
   );
--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,59 @@
+
+export class VisualizationContainer {
+    constructor() {
+        // create the dots
+        let visualizationDots = [];
+        for (let i=0; i<5; i++) {
+            visualizationDots.push(new Dot(new Vector2(i * 10, i * 10)));
+        }
+        this.visualizationDots = visualizationDots;
+    }
+
+    updateVisualization() {
+        for (let idx=0; idx<this.visualizationDots.length; idx++) {
+            this.visualizationDots[idx].tick(this.visualizationDots);
+        }
+    }
+}
+
+
+
 export class Dot {
-    constructor(Position) { // position is a position
+    constructor(Position) { // position is a vector2
         this.position = Position;
         this.forces = new Vector2(0,0);
         this.targetPosition = new Vector2(0,0);
     }
-  
-    getForces(ListOfDots, ) {
 
+    tick(visualizationDots) {
+        this.getForces(visualizationDots);
+        this.moveDot(new Vector2(2,2));
     }
   
+    getForces(visualizationDots) { // visualizationDots is a list of Dots
+        // find forces according to targetPosition
+        let distanceFromTargetPosition = new Vector2(this.targetPosition.X - this.position.X, this.targetPosition.Y - this.position.Y);
+        this.forces.X = distanceFromTargetPosition.X * 5; // 5 is how strongly the target position should pull on the dot
+        this.forces.Y = distanceFromTargetPosition.Y * 5;
+
+        for (const dot of visualizationDots) {
+            if (!dot == this) { // idk if this works?
+                let distanceFromDot = new Vector2(dot.position.X - this.position.X, dot.position.Y - this.position.Y);
+                this.forces.X += distanceFromDot.X * 0.5;
+                this.forces.Y += distanceFromDot.Y * 0.5;
+            }
+        }
+    }
+
+    moveDot(velocity) {
+        this.position.X += velocity.X;
+        this.position.Y += velocity.Y;
+    }
+
+    move(displacement) { // displacement is a vector2
+        this.position.X += displacement.X;
+        this.position.Y += displacement.Y;
+    }
 }
 
 
@@ -20,8 +65,9 @@ export class Vector2 { // represents a 2d vector.
 }
 
 
-export const Visualization = ({ visualizationDots }) => { // this generates a react component
+export const Visualization = ({ visualizationContainer }) => { // this generates a react component
     let htmlToReturn = [];
+    let visualizationDots = visualizationContainer.visualizationDots;
 
     for (let i=0; i<visualizationDots.length; i++) { // this is so cooked
         htmlToReturn.push(
